2c12c4f8acbbf219698a38d6a27b4d4f8c356978
Juraj Danko <sudanec@windowslive.com>
2023-02-27 15:30:40 +0100

    Release commit

--- sudanec WiFi Manager App/Form1.cs
+++ sudanec WiFi Manager App/Form1.cs
@@ -4,34 +4,168 @@ namespace sudanec_WiFi_Manager_App
     {
         public Form1()
         {
-            InitializeComponent();
-
-            loadNetworksToGrid();
-            WiFiData.getWiFiData();
+            try
+            { 
+                InitializeComponent();
+                button2.Enabled = CheckExcelPresent();
+                SetTooltips();
+                //Task.Run(LoadNetworksToGrid);
+                LoadNetworksToGrid();
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("System error when initializing the application: " + ex.Message, "sudanec WiFi Manager .::. Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
-        private void loadNetworksToGrid()
+        private void SetTooltips()
         {
-            System.Data.DataTable dt = new System.Data.DataTable();
-            dt.Columns.Add("Network", typeof(string));
-            dt.Columns.Add("Type", typeof(string));
-            dt.Columns.Add("Radio", typeof(string));
-            dt.Columns.Add("Vendor", typeof(string));
-            dt.Columns.Add("Auth", typeof(string));
-            dt.Columns.Add("Cipher", typeof(string));
-            dt.Columns.Add("Password", typeof(string));
-            dt.Columns.Add("Roaming", typeof(string));
-            dt.Columns.Add("Data Limit", typeof(string));
-            dt.Columns.Add("Over Data Limit", typeof(string));
+            try
+            {
+                System.Windows.Forms.ToolTip ToolTip1 = new System.Windows.Forms.ToolTip();
+                ToolTip1.SetToolTip(button1, "Opens the author's website with donate options");
+                System.Windows.Forms.ToolTip ToolTip2 = new System.Windows.Forms.ToolTip();
+                ToolTip1.SetToolTip(button2, "Exports data to Microsoft Excel (only available when Microsoft Excel is installed on the machine)");
+            }
+            catch { }
+        }
 
-            System.Data.DataRow row1 = dt.NewRow();
-            row1["Network"] = "Test";
-            dt.Rows.Add(row1);
+        private bool CheckExcelPresent()
+        {
+            try
+            {
+                Type officeType = Type.GetTypeFromProgID("Excel.Application");
+                if (officeType == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
 
-            dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader);
-            dataGridView1.ReadOnly = true;
-            dataGridView1.DataSource = dt;
-            //dataGridView1.DataBind();
+        private void LoadNetworksToGrid()
+        {
+            try
+            {
+                System.Threading.Thread.Sleep(5000);
+                System.Data.DataTable dt = new System.Data.DataTable();
+                dt.Columns.Add("Network", typeof(string));
+                dt.Columns.Add("Type", typeof(string));
+                dt.Columns.Add("Radio", typeof(string));
+                dt.Columns.Add("Vendor", typeof(string));
+                dt.Columns.Add("Auth", typeof(string));
+                dt.Columns.Add("Cipher", typeof(string));
+                dt.Columns.Add("Password", typeof(string));
+                //dt.Columns.Add("Roaming", typeof(string));
+                //dt.Columns.Add("Data Limit", typeof(string));
+                //dt.Columns.Add("Over Data Limit", typeof(string));
+
+                int i, j;
+                List<string> wifis = WiFiData.getAllWiFiProfiles();
+                List<string> wifiDetails = new List<string>();
+
+
+                for (i = 0; i < wifis.Count; i++)
+                {
+                    System.Data.DataRow row1 = dt.NewRow();
+                    wifiDetails = WiFiData.getWiFiDetails(wifis[i]);
+                    row1["Network"] = wifis[i];
+                    for(j = 0; j < wifiDetails.Count; j++) { row1[j+1] = wifiDetails[j]; }
+                    dt.Rows.Add(row1);
+                }
+
+                //var ctl = dataGridView1.AsyncState as System.Windows.Forms.Control;
+                //if (ctl != null && ctl.InvokeRequired)
+                //{
+                //    ctl.Invoke(new Action<IAsyncResult>(LoadNetworksToGrid), dataGridView1);
+                //} else
+                //{
+
+                    dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                    dataGridView1.AutoResizeColumns();
+                    dataGridView1.ReadOnly = true;
+                    dataGridView1.DataSource = dt;
+                //}
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Following error occurred when loading known WiFi profiles: " + ex.Message, "sudanec WiFi Manager .::. Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            { 
+                System.Diagnostics.ProcessStartInfo psi = new System.Diagnostics.ProcessStartInfo
+                {
+                    FileName = "cmd",
+                    Arguments = "/c start https://www.sudanec.com/donate"
+                };
+                System.Diagnostics.Process.Start(psi);
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Unable to open default web browser: " + ex.Message, "sudanec WiFi Manager .::. Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void copyAlltoClipboard()
+        {
+            try
+            {
+                dataGridView1.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableAlwaysIncludeHeaderText;
+                dataGridView1.SelectAll();
+                DataObject dataObj = dataGridView1.GetClipboardContent();
+                if (dataObj != null)
+                    Clipboard.SetDataObject(dataObj);
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Could not copy data from the table: " + ex.Message, "sudanec WiFi Manager .::. Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                dataGridView1.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableWithAutoHeaderText;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                copyAlltoClipboard();
+                Microsoft.Office.Interop.Excel.Application xlexcel;
+                Microsoft.Office.Interop.Excel.Workbook xlWorkBook;
+                Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet;
+                object misValue = System.Reflection.Missing.Value;
+                xlexcel = new Microsoft.Office.Interop.Excel.Application();
+                xlexcel.ScreenUpdating = false;
+                xlexcel.Visible = true;
+                xlWorkBook = xlexcel.Workbooks.Add(misValue);
+                xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+                Microsoft.Office.Interop.Excel.Range CR = (Microsoft.Office.Interop.Excel.Range)xlWorkSheet.Cells[1, 1];
+                CR.Select();
+                xlWorkSheet.PasteSpecial(CR, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, true);
+                xlWorkSheet.Columns[1].Delete();
+                xlWorkSheet.Columns.AutoFit();
+                xlWorkSheet.get_Range("A1").Select();
+                xlexcel.ScreenUpdating = true;
+
+                if (dataGridView1.Rows.Count > 0)
+                {
+                    dataGridView1.CurrentCell = dataGridView1[1, 0];
+                }
+            } catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("An error occurred when exporting to MS Excel: " + ex.Message, "sudanec WiFi Manager .::. Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
--- sudanec WiFi Manager App/WiFiData.cs
+++ sudanec WiFi Manager App/WiFiData.cs
@@ -8,24 +8,144 @@ namespace sudanec_WiFi_Manager_App
 {
     internal class WiFiData
     {
-        public static void getAllWiFiProfiles()
+        public static List<string> getAllWiFiProfiles()
         {
-            System.Diagnostics.Process cmd = new System.Diagnostics.Process();
-            cmd.StartInfo.FileName = "cmd.exe";
-            cmd.StartInfo.RedirectStandardInput = true;
-            cmd.StartInfo.RedirectStandardOutput = true;
-            cmd.StartInfo.CreateNoWindow = true;
-            cmd.StartInfo.UseShellExecute = false;
-            cmd.Start();
+            try
+            { 
+                System.Diagnostics.Process cmd = new System.Diagnostics.Process();
+                cmd.StartInfo.FileName = "cmd.exe";
+                cmd.StartInfo.RedirectStandardInput = true;
+                cmd.StartInfo.RedirectStandardOutput = true;
+                cmd.StartInfo.CreateNoWindow = true;
+                cmd.StartInfo.UseShellExecute = false;
+                cmd.Start();
 
-            cmd.StandardInput.WriteLine("netsh wlan show profiles");
-            cmd.StandardInput.Flush();
-            cmd.StandardInput.Close();
-            cmd.WaitForExit();
+                cmd.StandardInput.WriteLine("netsh wlan show profiles");
+                cmd.StandardInput.Flush();
+                cmd.StandardInput.Close();
+                cmd.WaitForExit();
 
-            string cmdOutput = cmd.StandardOutput.ReadToEnd();
-            cmdOutput = cmdOutput.Substring(cmdOutput.IndexOf(":"));
-            //System.Windows.Forms.MessageBox.Show(cmd.StandardOutput.ReadToEnd());
+                List<string> resultList = new List<string>();
+                string cmdOutput = cmd.StandardOutput.ReadToEnd();
+                int i = 0;
+
+                while (i > -1)
+                {
+                    i = cmdOutput.IndexOf(" : ");
+                    if (i > -1)
+                    {
+                        cmdOutput = cmdOutput.Substring(i + 3);
+                        resultList.Add(GetFirstLine(cmdOutput));
+                    }
+                }
+                //System.Threading.Thread.Sleep(5000);
+            
+                //System.Windows.Forms.MessageBox.Show(cmd.StandardOutput.ReadToEnd());
+                return resultList;
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Could not load WiFi profiles: " + ex.Message, "sudanec WiFi Manager .::. Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return new List<string>();
+            }
+        }
+
+        public static List<string> getWiFiDetails(string wifiProfile)
+        {
+            try
+            {
+                List<string> resultList = new List<string>();
+                System.Diagnostics.Process cmd = new System.Diagnostics.Process();
+                cmd.StartInfo.FileName = "cmd.exe";
+                cmd.StartInfo.RedirectStandardInput = true;
+                cmd.StartInfo.RedirectStandardOutput = true;
+                cmd.StartInfo.CreateNoWindow = true;
+                cmd.StartInfo.UseShellExecute = false;
+                cmd.Start();
+
+                cmd.StandardInput.WriteLine("netsh wlan show profile \"name=" + wifiProfile + "\" key=clear");
+                cmd.StandardInput.Flush();
+                cmd.StandardInput.Close();
+                cmd.WaitForExit();
+
+                string cmdOutput = cmd.StandardOutput.ReadToEnd();
+                if (cmdOutput.IndexOf("Type                   : ") > -1)
+                {
+                    cmdOutput = cmdOutput.Substring(cmdOutput.IndexOf("Type                   : ") + 25);
+                    resultList.Add(GetFirstLine(cmdOutput));
+                }
+                else resultList.Add("N/A");
+
+                if (cmdOutput.IndexOf("Radio type             : ") > -1)
+                {
+                    cmdOutput = cmdOutput.Substring(cmdOutput.IndexOf("Radio type             : ") + 25);
+                    resultList.Add(GetFirstLine(cmdOutput));
+                }
+                else resultList.Add("N/A");
+
+                if (cmdOutput.IndexOf("Vendor extension          : ") > -1)
+                {
+                    cmdOutput = cmdOutput.Substring(cmdOutput.IndexOf("Vendor extension          : ") + 28);
+                    resultList.Add(GetFirstLine(cmdOutput));
+                }
+                else resultList.Add("N/A");
+
+                if (cmdOutput.IndexOf("Authentication         : ") > -1)
+                {
+                    cmdOutput = cmdOutput.Substring(cmdOutput.IndexOf("Authentication         : ") + 25);
+                    resultList.Add(GetFirstLine(cmdOutput));
+                }
+                else resultList.Add("N/A");
+
+                string tempCipher = "";
+                if (cmdOutput.IndexOf("Cipher                 : ") > -1)
+                {
+                    cmdOutput = cmdOutput.Substring(cmdOutput.IndexOf("Cipher                 : ") + 25);
+                    tempCipher = GetFirstLine(cmdOutput);
+
+                    while (cmdOutput.IndexOf("Cipher                 : ") > -1)
+                        {
+                            cmdOutput = cmdOutput.Substring(cmdOutput.IndexOf("Cipher                 : ") + 25);
+                            tempCipher = tempCipher + ", " + GetFirstLine(cmdOutput);
+                        }
+
+                    resultList.Add(tempCipher);
+                }
+                else resultList.Add("N/A");
+
+                if (cmdOutput.IndexOf("Key Content            : ") > -1)
+                {
+                    cmdOutput = cmdOutput.Substring(cmdOutput.IndexOf("Key Content            : ") + 25);
+                    resultList.Add(GetFirstLine(cmdOutput));
+                }
+                else resultList.Add("N/A");
+
+                return resultList;
+            } catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Could not get WiFi details for network " + wifiProfile + ": " + ex.Message, "sudanec WiFi Manager .::. Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return new List<string>();
+            }
+        }
+
+        static string GetFirstLine(string text)
+        {
+            try
+            {
+                using (var reader = new StringReader(text))
+                {
+                    return ManageString(reader.ReadLine());
+                }
+            }
+            catch (Exception ex)
+            {
+                return "N/A";
+            }
+        }
+
+        static string ManageString(string text)
+        {
+            return text;
         }
     }
 }
